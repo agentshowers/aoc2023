20b41f8edcc13130dd66d95cf2d35984c8eaeb42
Joao Amorim <i.psyduck@gmail.com>
2023-12-01 11:08:38 +0000

    clean

--- solutions/1.rb
+++ solutions/1.rb
@@ -21,9 +21,8 @@ class Day1 < Base
   private def solve_one
     sum = 0
     @input.each do |s|
-      numbers = s.scan(/\d/)
-      sum += numbers[0].to_i*10
-      sum += numbers.last.to_i
+      l, r = digits(s)
+      sum += (l*10 + r)
     end
     sum
   end
@@ -31,38 +30,30 @@ class Day1 < Base
   private def solve_two
     sum = 0
     @input.each do |s|
-      first_idx = nil
-      first_n = nil
-      last_idx = nil
-      last_n = nil
-
-      NUMBERS.each_with_index do |n, i|
-        l_idx = s.index(n)
-        r_idx = s.rindex(n)
-        if l_idx && (!first_idx || l_idx < first_idx)
-          first_idx = l_idx
-          first_n = i + 1
-        end
-        if r_idx && (!last_idx || r_idx > last_idx)
-          last_idx = r_idx
-          last_n = i + 1
-        end
-      end
-      (1..9).to_a.each do |n|
-        l_idx = s.index(n.to_s)
-        r_idx = s.rindex(n.to_s)
-        if l_idx && (!first_idx || l_idx < first_idx)
-          first_idx = l_idx
-          first_n = n
-        end
-        if r_idx && (!last_idx || r_idx > last_idx)
-          last_idx = r_idx
-          last_n = n
-        end
-      end
-      real = first_n.to_i*10 + last_n.to_i
-      sum += real
+      l, r = digits(s, true)
+      sum += (l*10 + r)
     end
     sum
   end
+
+  private def digits(s, text = false)
+    left, right, l_digit, r_digit = nil
+
+    (1..9).to_a.each do |n|
+      l_idx = text ? s.index(/#{n}|#{NUMBERS[n-1]}/) : s.index(n.to_s)
+      r_idx = text ? s.rindex(/#{n}|#{NUMBERS[n-1]}/) : s.rindex(n.to_s)
+
+      if l_idx && (!left || l_idx < left)
+        left = l_idx
+        l_digit = n
+      end
+
+      if r_idx && (!right || r_idx > right)
+        right = r_idx
+        r_digit = n
+      end
+    end
+    [l_digit, r_digit]
+  end
+
 end
