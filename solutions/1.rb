44aa9d167f06bce3d736c498a107c898969b5f12
Joao Amorim <i.psyduck@gmail.com>
2023-12-01 07:16:00 +0000

    day 1 ugly

--- solutions/1.rb
+++ solutions/1.rb
@@ -4,6 +4,7 @@ require "./lib/base.rb"
 
 class Day1 < Base
   DAY = 1
+  NUMBERS = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
 
   def initialize(type = "example")
     @input = Parser.lines(DAY, type)
@@ -18,10 +19,50 @@ class Day1 < Base
   end
 
   private def solve_one
-    Utils.sum(@input[0].to_i, @input[1].to_i)
+    sum = 0
+    @input.each do |s|
+      numbers = s.scan(/\d/)
+      sum += numbers[0].to_i*10
+      sum += numbers.last.to_i
+    end
+    sum
   end
 
   private def solve_two
-    Utils.sum(@input[0].to_i, @input[1].to_i)
+    sum = 0
+    @input.each do |s|
+      first_idx = nil
+      first_n = nil
+      last_idx = nil
+      last_n = nil
+
+      NUMBERS.each_with_index do |n, i|
+        l_idx = s.index(n)
+        r_idx = s.rindex(n)
+        if l_idx && (!first_idx || l_idx < first_idx)
+          first_idx = l_idx
+          first_n = i + 1
+        end
+        if r_idx && (!last_idx || r_idx > last_idx)
+          last_idx = r_idx
+          last_n = i + 1
+        end
+      end
+      (1..9).to_a.each do |n|
+        l_idx = s.index(n.to_s)
+        r_idx = s.rindex(n.to_s)
+        if l_idx && (!first_idx || l_idx < first_idx)
+          first_idx = l_idx
+          first_n = n
+        end
+        if r_idx && (!last_idx || r_idx > last_idx)
+          last_idx = r_idx
+          last_n = n
+        end
+      end
+      real = first_n.to_i*10 + last_n.to_i
+      sum += real
+    end
+    sum
   end
 end
