f5c56467652fb13183b9f56419d7fce56f8f233a
Joao Amorim <i.psyduck@gmail.com>
2023-12-12 14:57:43 +0000

    20 minute solution :lolsob:

--- solutions/12.rb
+++ solutions/12.rb
@@ -7,33 +7,41 @@ class Day12 < Base
 
   def initialize(type = "example")
     @lines = Parser.lines(DAY, type)
-    @groups = @lines.map do |line|
-      springs, ns = line.split(" ")
-      split_springs = springs.gsub(".", " ").strip.split(" ").map(&:strip)
-      [split_springs, ns.split(",").map(&:to_i)]
-    end
+    #@solutions = File.readlines("pyout.txt", chomp: true).map(&:to_i)
+    @combos_memo = {}
+    @single_combos_memo = {}
   end
 
   def one
-    #s = ""
-    @groups.each_with_index.map do |g, i| 
-      v = solve(*g)
-      #s += "#{@lines[i]} #{g.to_s} : #{v}\n"
-      v
+    groups = @lines.map { |line| parse(line) }
+    groups.map do |g| 
+      solve(*g)
     end.sum
-    #File.write("output.txt", s)
-    #1
   end
 
   def two
-    0 #@groups.to_s
+    groups = @lines.map { |line| parse(line, 5) }
+    groups.map do |g| 
+      v = solve(*g)
+      puts v
+      v
+    end.sum
+  end
+
+  def parse(line, replicate = 1)  
+    springs, ns = line.split(" ")
+    springs = Array.new(replicate){ springs.dup }.join("?")
+    split_springs = springs.gsub(".", " ").strip.split(" ").map(&:strip)
+    numbers = ns.split(",").map(&:to_i)
+    numbers = numbers * replicate
+    [split_springs, numbers]
   end
 
   def solve(str, sizes)
     breaks = size_breaks(str, sizes)
     breaks.map do |b|
       b.zip(str).map do |ns, s|
-        combos(s, ns)
+        cached_combos(s, ns)
       end.inject(:*)
     end.sum
   end
@@ -64,24 +72,32 @@ class Day12 < Base
     solutions
   end
 
+  def cached_combos(str, ns)
+    key = "#{str}-#{ns.join(",")}"
+    return @combos_memo[key] if @combos_memo[key]
+    res = combos(str, ns)
+    @combos_memo[key] = res
+    res
+  end
+
   def combos(str, ns)
     return 0 if str.include?("#") && ns.length == 0
     return 0 if str.length < (ns.sum + ns.length - 1)
     return 1 if ns.length == 0
-    return single_combo(str, ns[0]) if ns.length == 1
+    return cached_single_combo(str, ns[0]) if ns.length == 1
   
     n = ns[0]
     rem = ns[1..-1]
     if str[0] == "#"
       return 0 if str[n] == "#"
-      combos(str[n+1..-1], ns[1..-1])
+      cached_combos(str[n+1..-1], ns[1..-1])
     else
       left_hash = str.index("#")
       sum = 0
       i = n
       loop do
         if str[i] != "#"
-          sum += (single_combo(str[i-n..i-1], n) * combos(str[i+1..-1], rem))
+          sum += (cached_single_combo(str[i-n..i-1], n) * cached_combos(str[i+1..-1], rem))
         end
         i += 1
         break if left_hash && i - n > left_hash
@@ -91,12 +107,22 @@ class Day12 < Base
     end
   end
 
+  def cached_single_combo(str, n)
+    key = "#{str}-#{n}"
+    return @single_combos_memo[key] if @single_combos_memo[key]
+    res = single_combo(str, n)
+    @single_combos_memo[key] = res
+    res
+  end
+
   def single_combo(str, n)
     return 1 if str.length == n
     l_idx = str.index("#")
     return str.length - n + 1 unless l_idx
     r_idx = str.rindex("#")
     return 0 if r_idx - l_idx + 1 > n
+    return str.length - n + 1if l_idx >= str.length - n && r_idx < n
+
     used = r_idx - l_idx + 1
     rem = n - used
     left_moves = [l_idx, n - used].min
