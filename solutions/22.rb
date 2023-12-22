8d55b634928627bf5d1de05e35c4443be53d070b
Joao Amorim <i.psyduck@gmail.com>
2023-12-22 15:28:27 +0000

    rejig 22

--- solutions/22.rb
+++ solutions/22.rb
@@ -19,12 +19,10 @@ class Day22 < Base
   end
 
   def one
-    @core_pieces = @supports.select { |x| x.length == 1 && x[0] != -1 }.map { |x| x[0] }.uniq
     @pieces.length - @core_pieces.length
   end
 
   def two
-    arr = Array.new(@pieces.length) { -1 }
     @core_pieces.map do |idx|
       dropping_pieces(idx)
     end.sum
@@ -37,7 +35,7 @@ class Day22 < Base
     
     while dropping.any?
       dropped = dropping.shift
-      (@supporters[dropped] || []).each do |block|
+      (@dependents[dropped] || []).each do |block|
         supports[block].delete(dropped)
         if supports[block].length == 0
           dropping << block
@@ -52,56 +50,62 @@ class Day22 < Base
   # Drop logic
 
   def unfreeze
-    @stack = []
-    @stack << Array.new(MAX) { Array.new(MAX) { GROUND } }
     @supports = Array.new(@pieces.length) { [] }
-    @supporters = {}
-    @pieces.each_with_index do |piece, idx|
-      drop(piece, idx)
+    @dependents = {}
+    @core_pieces = []
+
+    stack = []
+    stack << Array.new(MAX) { Array.new(MAX) { GROUND } }
+    (0..@pieces.length-1).each do |idx|
+      drop(idx, stack)
     end
   end
 
-  def drop(piece, idx)
-    min_x = piece[0][0]
-    min_y = piece[0][1]
-    min_z = piece[0][2]
-    max_x = piece[1][0]
-    max_y = piece[1][1]
-    max_z = piece[1][2]
+  def drop(idx, stack)
+    piece = @pieces[idx]
+    min_x, min_y, min_z = piece[0]
+    max_x, max_y, max_z = piece[1]
+    supports = []
 
-    under = []
     loop do
-      under = find_at(min_z-1, min_x, max_x, min_y, max_y)
-      break if under.length > 0
+      if stack.length > min_z - 1
+        supports = find_at(stack, min_z-1, min_x, max_x, min_y, max_y)
+        break if supports.length > 0
+      end
       min_z -= 1
       max_z -= 1
     end
 
-    under.each do |under_idx|
-      @supporters[under_idx] ||= []
-      @supporters[under_idx] << idx
+    update_stack(stack, idx, min_x, min_y, min_z, max_x, max_y, max_z)
+
+    supports.each do |sup_idx|
+      @dependents[sup_idx] ||= []
+      @dependents[sup_idx] << idx
     end
-    @supports[idx] = under
-    (min_z..max_z).each do |z|
-      if @stack.length <= z
-        @stack << Array.new(MAX) { Array.new(MAX) { EMPTY } }
-      end
-      (min_x..max_x).each do |x|
-        (min_y..max_y).each do |y|
-          @stack[z][x][y] = idx
-        end
-      end
-    end
+    @supports[idx] = supports
+    @core_pieces << supports[0] if supports[0] != GROUND && supports.length == 1 && !@core_pieces.include?(supports[0])
   end
 
-  def find_at(z, min_x, max_x, min_y, max_y)
-    return [] if @stack.length <= z
+  def find_at(stack, z, min_x, max_x, min_y, max_y)
     blocks = []
-    @stack[z][min_x..max_x].each do |row|
+    stack[z][min_x..max_x].each do |row|
       row[min_y..max_y].each do |block|
         blocks << block if block
       end
     end
     blocks.uniq
+  end
+
+  def update_stack(stack, idx, min_x, min_y, min_z, max_x, max_y, max_z)
+    (min_z..max_z).each do |z|
+      if stack.length <= z
+        stack << Array.new(MAX) { Array.new(MAX) { EMPTY } }
+      end
+      (min_x..max_x).each do |x|
+        (min_y..max_y).each do |y|
+          stack[z][x][y] = idx
+        end
+      end
+    end
   end
 end
